6f5e21b405b51290d883f901b028d7da9f708e90
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-06-16 15:49:10 +0530

    partner added

---
+++ src/Components/Home/Partner.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function Partner() {
+    return (
+        <section className="bg-primary p-5 text-white">
+            <div className="container text-center">
+                <h1>Partner With us</h1>
+            </div>
+                <div className="container">
+                    <div className="row mt-3">
+                        <div className="col-lg-4 text-center">
+                            <h4><b>Registered Office</b></h4>
+                            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit proin leo ornare!</p>
+                        </div>
+                        <div className="col-lg-4 text-center">
+                            <h4><b>Registered Office</b></h4>
+                            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit proin leo ornare!</p>
+                        </div>
+                        <div className="col-lg-4 text-center">
+                            <h4><b>Registered Office</b></h4>
+                            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit proin leo ornare!</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="container text-center">
+                <p><b>Lorem ipsum dolor sit amet consectetur adipiscing elit proin leo ornare!</b></p>
+                </div>
+        </section>
+    )
+}
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,6 +3,7 @@ import HomeSlider from '../Components/Home/HomeSlider'
 import Services from '../Components/Home/Services'
 import Why from '../Components/Home/Why'
 import Experience from '../Components/Home/Experience'
+import Partner from '../Components/Home/Partner'
 
 export default function Home() {
     return (
@@ -11,6 +12,7 @@ export default function Home() {
             <Services />
             <Why />
             <Experience />
+            <Partner />
         </div>
     )
 }
