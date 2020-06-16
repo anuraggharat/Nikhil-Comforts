1fdefa4db8f24460ed129999dde51051321046a9
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-06-16 13:06:49 +0530

    navbar strucure done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer';
+import Home from './Pages/Home';
 
 function App() {
   return (
     <div>
-      
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route to="/" component={Home} />
+        </Switch>
+        <Footer />
+      </Router>
+
+
     </div>
   );
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,146 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
+
+const MainNavbar = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Nikhil Comforts</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/">HOME</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                SERVICES
+              </DropdownToggle>
+              <DropdownMenu right>
+              <DropdownItem>
+                HVAC
+                </DropdownItem>
+                <DropdownItem>
+                MEP
+                </DropdownItem>
+                <DropdownItem>
+                INTERIORS
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  ENERGY CONSULTATION
+                </DropdownItem>
+                <DropdownItem>
+                  RETRO FITTING
+                </DropdownItem> 
+               <DropdownItem>
+                PUBLIC HEALTH ENGINEERING BMS
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                PROJECTS
+              </DropdownToggle>
+              <DropdownMenu right>
+              <DropdownItem>
+                HVAC
+                </DropdownItem>
+                <DropdownItem>
+                MEP
+                </DropdownItem>
+                <DropdownItem>
+                INTERIORS
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  ENERGY CONSULTATION
+                </DropdownItem>
+                <DropdownItem>
+                  RETRO FITTING
+                </DropdownItem> 
+               <DropdownItem>
+                PUBLIC HEALTH ENGINEERING BMS
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+            <NavItem>
+              <NavLink href="#">ABOUT US</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                BLOGS
+              </DropdownToggle>
+              <DropdownMenu right>
+              <DropdownItem>
+                HVAC
+                </DropdownItem>
+                <DropdownItem>
+                MEP
+                </DropdownItem>
+                <DropdownItem>
+                INTERIORS
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  ENERGY CONSULTATION
+                </DropdownItem>
+                <DropdownItem>
+                  RETRO FITTING
+                </DropdownItem> 
+               <DropdownItem>
+                PUBLIC HEALTH ENGINEERING BMS
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                CAREER
+              </DropdownToggle>
+              <DropdownMenu right>
+              <DropdownItem>
+                HVAC
+                </DropdownItem>
+                <DropdownItem>
+                MEP
+                </DropdownItem>
+                <DropdownItem>
+                INTERIORS
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  ENERGY CONSULTATION
+                </DropdownItem>
+                <DropdownItem>
+                  RETRO FITTING
+                </DropdownItem> 
+               <DropdownItem>
+                PUBLIC HEALTH ENGINEERING BMS
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default MainNavbar;
