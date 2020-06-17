f4c2ab07eae88b714206108d9b0dc8001f8caa96
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-06-17 17:28:43 +0530

    new icons added

--- src/Components/Home/Why.js
+++ src/Components/Home/Why.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import Heading from '../Heading'
 import HomeCard from '../HomeCard'
+import Img2 from '../../Images/Adherence_to_schedule.svg'
+import Img3 from '../../Images/Awesome_Support.svg'
+import Img1 from '../../Images/Fastest_Service.svg'
+import Img4 from '../../Images/Persuit_to_Excellence.svg'
+
 export default function Why() {
     return (
         <section className="bg-light p-5">
                 <Heading title="Why Choose Us?" />
                 <div className="container p-5">
                     <div className="row px-5">
-                        <HomeCard />
-                        <HomeCard />
-                        <HomeCard />
-                        <HomeCard />
+                        <HomeCard imgsource={Img1} title="Fastest Service" />
+                        <HomeCard imgsource={Img2} title="Adherence To Schedule"/>
+                        <HomeCard imgsource={Img3}  title="Awesome Support"/>
+                        <HomeCard imgsource={Img4}  title="Pursuit To Excellence"/>
                     </div>
                 </div>
         </section>
--- src/Components/HomeCard.js
+++ src/Components/HomeCard.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import {FcServices} from 'react-icons/fc'
 
-export default function HomeCard({children}) {
+export default function HomeCard({imgsource,title}) {
     return (
-        <div className="col-lg-3 col-sm-6">
+        <div className="col-lg-3 col-sm-6 px-5 text-center">
         <div className="row py-2">
-            <div className="col-lg-4 col-sm-6">
-                <FcServices  className="display-4"/>
+            <div className="col-lg-12 col-sm-12 px-4">
+                <img src={imgsource} alt="Card" className="img-fluid w-75"  />
             </div>
-            <div className="col-lg-8 col-sm-6 align-middle text-left">
-                <h4>Fastest Service</h4>
+            <div className="col-lg-12 col-sm-12 align-middle text-center mt-4">
+            <h4>{title}</h4>
             </div>
         </div>
         <div className="row">
+        <p className="text-muted">
         Lorem ipsum dolor sit amet consectetur adipiscing elit proin leo ornare!
+
+        </p>
         </div>
     </div>
     )
