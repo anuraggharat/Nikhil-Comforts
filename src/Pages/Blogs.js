da0f8620dcebd0bdd75ec12d24ed2aca2f0868d9
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-06-17 22:09:49 +0530

    blogs page added

--- src/Pages/Blogs.js
+++ src/Pages/Blogs.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Jumbotron,Breadcrumb, BreadcrumbItem } from 'reactstrap'
+import { Jumbotron,Breadcrumb, BreadcrumbItem ,ListGroup, ListGroupItem,Badge  } from 'reactstrap'
 import BlogCard from '../Components/Blogs/BlogCard'
 
 export default function Blogs() {
@@ -13,9 +13,9 @@ export default function Blogs() {
                 </Breadcrumb>
             </Jumbotron>
             <section>
-                <div className="container">
+                <div className="container pb-3">
                     <div className="row">
-                        <div className="col-lg-8 col-sm-12">
+                        <div className="col-lg-9 col-sm-12">
                             <BlogCard />
                             <BlogCard />
                             <BlogCard />
@@ -23,9 +23,35 @@ export default function Blogs() {
                             <BlogCard />
                             <BlogCard />
                             <BlogCard />
-
                         </div>
-                        <div className="col-lg-4 col-sm-12"></div>
+                        <div className="col-lg-3 col-sm-12 pt-5">
+                            <h2>Categories</h2>
+                            <ListGroup flush>
+                            <ListGroupItem  tag="a" href="#">Cras justo odio</ListGroupItem>
+                            <ListGroupItem tag="a" href="#">Dapibus ac facilisis in</ListGroupItem>
+                            <ListGroupItem tag="a" href="#">Morbi leo risus</ListGroupItem>
+                            <ListGroupItem tag="a" href="#">Porta ac consectetur ac</ListGroupItem>
+                            <ListGroupItem tag="a" href="#">Vestibulum at eros</ListGroupItem>
+                            </ListGroup>
+                            <div className=" advertisement ">
+                                <div className="overlay pt-5">
+                                <p className="align-middle">Lorem epsum is a dummy text</p>
+                                <button className="btn btn-primary">Buy Now</button>
+                                </div>
+                            </div>
+                            <div className="mt-5">
+                                <h2>Archives</h2>
+                            <ListGroup flush>
+                            <ListGroupItem className="justify-content-between">Cras justo odio <Badge pill>14</Badge></ListGroupItem>
+                            <ListGroupItem className="justify-content-between">Dapibus ac facilisis in <Badge pill>2</Badge></ListGroupItem>
+                            <ListGroupItem className="justify-content-between">Morbi leo risus <Badge pill>1</Badge></ListGroupItem>
+                            <ListGroupItem className="justify-content-between">Cras justo odio <Badge pill>14</Badge></ListGroupItem>
+                            <ListGroupItem className="justify-content-between">Dapibus ac facilisis in <Badge pill>2</Badge></ListGroupItem>
+                            <ListGroupItem className="justify-content-between">Morbi leo risus <Badge pill>1</Badge></ListGroupItem>
+                            
+                            </ListGroup>
+                            </div>
+                        </div>
 
                     </div>
                 </div>
